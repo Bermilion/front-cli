68ba7cc66b227202019f5b162f338d88992a3722
volkov.i <volkov.i@studio2i.ru>
2019-04-23 18:12:21 +0300

    начал добавлять функционал редактирования конфига

--- commands/config.py
+++ commands/config.py
@@ -1,12 +1,49 @@
 # -*- coding: utf-8 -*-
 
+import os
 import click
+import json
+
+env = os.environ
+directory = env['HOME'] + '/.front-cli'
+
+
+def getConfig():
+    os.chdir(directory)
+    with open('conf.json', 'r') as file:
+        return json.load(file)
+
+def setConfig(data):
+    with open('conf.json', 'w') as file:
+        json.dump(data, file, sort_keys=True, indent=4)
+
+def parseJsonToStr(data):
+    return json.dumps(data, sort_keys=True, indent=4)
 
 @click.command()
-def config():
-    """чыва фыва фыва ыва ы"""
-    # print ('conf')
-    click.echo('Configurate')
+@click.option('--sql-user', default=False, help='Замена юзера')
+@click.option('--sql-password', default=False, help='Замена пароля')
+@click.option('--root-dir', default=False, help='Замена корневой директории проектов')
+@click.option('--list', '-l', is_flag=True, help='Показать конфиг')
+def config(sql_user, sql_password, root_dir, list):
+    """Служит для редактирования конфига"""
+    click.echo((sql_user, sql_password, root_dir, list))
 
-# if __name__ == '__main__':
-#     config()
+    if sql_user:
+        click.secho(sql_user, fg='green')
+        user = getConfig()
+        user['mysql']['login'] = sql_user
+        setConfig(user)
+
+    if sql_password:
+        click.secho(sql_password, fg='green')
+        password = getConfig()
+        password['mysql']['password'] = sql_password
+        setConfig(password)
+
+    if root_dir:
+        click.echo('none')
+        click.secho(root_dir, fg='green')
+
+    if list:
+        click.secho('{}'.format(parseJsonToStr(getConfig())), fg='green')
